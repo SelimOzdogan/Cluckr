ac17cc84fa6a7edba18ac1046bdc6c0336b93882
Selim Ozdogan <selimozdogan@hotmail.com>
2020-04-14 10:17:43 -0700

    Setup Server

--- app.js
+++ app.js
@@ -3,7 +3,18 @@ const express = require("express");
 const logger = require("morgan");
 const app = express();
 
+app.set("view engine", "ejs");
 app.use(logger("dev"));
 
 app.use(express.static(path.join(__dirname, "public")));
+app.use(express.urlencoded({ extended: true }));
 
+app.get("/", (req, res) => {
+  res.send("Under the Construction");
+});
+
+const PORT = 8080;
+const ADDRESS = "localhost";
+app.listen(PORT, ADDRESS, () => {
+  console.log(`Server listenning on http://${ADDRESS}:${PORT}`);
+});
