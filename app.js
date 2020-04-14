c03e1abdcfe45ab927cf3f8bc04e6e390d7632d6
Selim Ozdogan <selimozdogan@hotmail.com>
2020-04-14 12:48:22 -0700

    Finished : Part 2: SignIn

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 const path = require("path");
 const express = require("express");
 const logger = require("morgan");
+const cookieParser = require('cookie-parser')
+
 const app = express();
 
 app.set("view engine", "ejs");
 app.use(logger("dev"));
+
+app.use(cookieParser())
 
 app.use(express.static(path.join(__dirname, "public")));
 app.use(express.urlencoded({ extended: true }));
@@ -12,16 +16,29 @@ app.use(express.urlencoded({ extended: true }));
 // app.get("/", (req, res) => {
 //   res.send("Under the Construction");
 // });
-app.get("/", (req, res) => {
-    res.render("index");
-  });
+app.get("/", (request, response) => {
+    response.render("index");
+});
 
-app.get("/signIn", (req, res) => {
-    res.render("sign_in");
-  });
+app.get("/signIn", (request, response) => {
+    response.render("sign_in", { request });
+});
+
+app.post('/signIn', (request, response) => {
+    const { userName } = request.body;
+    const oneDay = 1000 * 60 * 60 * 24;
+    response.cookie('userName', userName, { maxAge: oneDay })
+    response.render("sign_in", { request });
+})
+
+app.delete('/signIn', (request, response) => {
+    response.clearCookie('userName')
+    response.render("sign_in");
+})
+
 
 const PORT = 8080;
 const ADDRESS = "localhost";
 app.listen(PORT, ADDRESS, () => {
-  console.log(`Server listenning on http://${ADDRESS}:${PORT}`);
+    console.log(`Server listenning on http://${ADDRESS}:${PORT}`);
 });
